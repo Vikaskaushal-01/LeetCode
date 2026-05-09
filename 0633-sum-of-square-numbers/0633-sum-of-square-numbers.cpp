c5921e74b63268d4f76025c8b21edb90a70c3099
Vikaskaushal-01 <kaushalvikas28@gmail.com>
2026-05-09 12:11:40 +0530

    Time: 0 ms (100%), Space: 7.7 MB (54.82%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long left = 0;
+        long right = sqrt(c);
+        
+        while (left <= right) {
+            long currentSum = left * left + right * right;
+            
+            if (currentSum == c) {
+                return true;
+            } else if (currentSum < c) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        
+        return false;
+    }
+};
