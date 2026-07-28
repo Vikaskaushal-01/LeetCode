ea6e2170a8520925f672d3e9e2128f6443b83585
Vikaskaushal-01 <kaushalvikas28@gmail.com>
2026-07-28 21:40:03 +0530

    Time: 7 ms (28.69%), Space: 12.6 MB (30.16%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        // Value-symbol pairs in descending order, including subtractive forms
         vector<pair<int, string>> roman = {
             {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
             {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
